8705ecc09c514f969f2058971fdf95595709a07b
shayanpzp <shayanpzp1@gmail.com>
2023-12-05 08:33:13 +0330

    chore : create Image and Comment models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from other_app.models import User  
+from other_app.models import Item
 
-# Create your models here.
+
+
+class Image(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    id_Items = models.ForeignKey(Item, on_delete=models.CASCADE)
+    id_User = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    id_Items = models.ForeignKey(Item, on_delete=models.CASCADE)
+    id_User = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
