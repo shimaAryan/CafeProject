e69eeac992dac14dda3af7e8d064b2e548ffd254
fariba_r <faribarezaee321@gmail.com>
2023-12-05 23:54:27 +0330

    init: create items and categoryitemes models

--- cafe/models.py
+++ cafe/models.py
@@ -1,12 +1,39 @@
 from django.db import models
 from account.models import User
 import datetime as dt
+
+class CategoryMeno(models.Model):
+    title = models.CharField(max_length=50)
+    SERVINGTIME=[
+        ("M","morning"),
+        ("N","noon"),
+        ("E","evening"),
+        ("N","night")
+    ]
+    serving_time=name = models.CharField(max_length=7,choices=SERVINGTIME)
+
+class Items(models.Model):
+    category_id = models.ForeignKey(CategoryMeno, on_delete=models.CASCADE,related_name="items")
+    title = models.CharField(max_length=100)
+    price=models.FloatField()
+    deccription=models.TextField(max_length=255)
+    status=models.BooleanField()
+    LIKES=[
+        ("1","very bad"),
+        ("2","bad"),
+        ("3","good"),
+        ("4","very good"),
+        ("5","extra good"),
+        
+    ]
+    likes=models.CharField(max_length=10,choices=LIKES)
+                             
 class Order(models.Model):
     order_time = models.DateTimeField(auto_now_add=True)
     user_order = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="user_order")
     delivery = models.DecimalField(max_digits=10, decimal_places=3, null=True, blank=True)
     delivery_time = models.TimeField(null=True, blank=True, default=dt.time(00, 00))
-    items = models.ManyToManyField(Item, blank=True, related_name="item_order", through='IntermediateModel')
+    items = models.ManyToManyField(Items, blank=True, related_name="item_order", through='IntermediateModel')
 
     class Meta:
         ordering = ['-order_time']
@@ -19,4 +46,5 @@ class ItemOrder(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order")
     items = models.ForeignKey(Items, on_delete=models.CASCADE, related_name="items")
     number_of_items = models.PositiveIntegerField()
+
 
