c176443855ea7c1db9e7f2a2ea128d5faa880a54
Sahar Mahmoudishad <sahar.mahmoodi01@gmail.com>
2023-12-08 11:50:54 +0330

    refactor: modify login template

--- cafe/admin.py
+++ cafe/admin.py
@@ -1,32 +1,32 @@
-# from django.contrib import admin
-# from .models import *
-#
-# admin.site.register(Like)
-# admin.site.register(Receipt)
-#
-#
-# @admin.register(Items)
-# class ItemsAdmin(admin.ModelAdmin):
-#     list_display = ['title']
-#
-#
-# @admin.register(CategoryMenu)
-# class CategoryMenuAdmin(admin.ModelAdmin):
-#     list_display = ['title']
-#
-#
-# @admin.register(Order)
-# class OrderAdmin(admin.ModelAdmin):
-#     list_display = []
-#
-#
-# class ItemAdmin(admin.ModelAdmin):
-#     list_display = ['title']
-#
-#
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Like)
+admin.site.register(Receipt)
+
+
+@admin.register(Items)
+class ItemsAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+@admin.register(CategoryMenu)
+class CategoryMenuAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = []
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
 # @admin.register(CategoryMenu)
 # class CategoryAdmin(admin.ModelAdmin):
 #     list_display = ['title']
 #
 #
-# admin.site.register(Order)
+# # admin.site.register(Order)
