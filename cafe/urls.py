f7984f3cac377a59050c4ed16464f268d60ca6ba
fariba_r <faribarezaee321@gmail.com>
2023-12-09 14:48:31 +0330

    init: complete and initialize the detail and menu page

--- cafe/urls.py
+++ cafe/urls.py
@@ -6,4 +6,7 @@ from django.contrib.auth import views as auth_views
 app_name = 'cafe'
 urlpatterns = [
     path('cart/', OrderItemView.as_view(), name='cart'),
+    path('menu/', CategoryItems.as_view(), name='menu'),
+    path('detail_item/<int:pk>', DetailItemView.as_view(), name='detail_item'),
+
 ]
--- cafe/views.py
+++ cafe/views.py
@@ -1,16 +1,18 @@
+from typing import Any
+from django.db import models
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import ListView, View
+from django.views.generic import ListView, View,DetailView
 from django.contrib import messages
 from .models import Order,CategoryMenu,Items,Receipt
 from cafe.forms.cart_form import OrderForm
-
+from core.models import Image
 
 class OrderItemView(LoginRequiredMixin, ListView):
     model = Order
-    template_name = "base.html"
-    # template_name = "cart.html"
+    
+    template_name = "cart.html"
     context_object_name = "item_orders"
     success_url = reverse_lazy("User:profile")
     form_class = OrderForm
@@ -24,22 +26,42 @@ class OrderItemView(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['all_order_items'] = [order.items for order in context['item_orders']]
         context['form'] = self.form_class
-        print(context['all_order_items'])
+        
         return context
+    
 
 
-class CategoryItems(LoginRequiredMixin,ListView):
+class CategoryItems(ListView):
     model = CategoryMenu
-    context_object_name = 'category'
-    template_name='category.html'
+    context_object_name = "categorys"
+    template_name='menu1.html'
 
     def get_queryset(self):
-        return self.model.objects.all()
+        return self.model.objects.prefetch_related("items").all()
 
     def get_context_data(self,*, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        for category in context["category"]:
-            context[category.title]=Items.objects.filter(category_id=category.id)
+        
+        context["images"]=Image.objects.all
+
+        return context
+class DetailItemView(LoginRequiredMixin,DetailView):
+    model=Items
+    context_object_name="item"
+    template_name="detail_item.html"
+   
+    def get_object(self, queryset=None):
+        idd = self.kwargs.get('pk')
+       
+        return Items.objects.get(id=idd)
+
+    def get_context_data(self,*, object_list=None, **kwargs):
+            context = super().get_context_data(**kwargs)
+            
+            context["image"]=Image.objects.get(object_id=self.kwargs.get('pk'))
+
+            return context
+
 class ReceiptView(LoginRequiredMixin, View):
     template_name = 'receipt.html'
     context_object_name = 'receipt'
