cd2f44a9b012e2e60505673ceb039b9ddccfe99c
Sahar Mahmoudishad <sahar.mahmoodi01@gmail.com>
2023-12-11 13:01:20 +0330

    fix:debuging

--- cafe/urls.py
+++ cafe/urls.py
@@ -1,16 +1,11 @@
-
 from django.urls import path
 from .views import *
 from django.contrib.auth import views as auth_views
 
 app_name = 'cafe'
 
-
-
-
-
 urlpatterns = [
-    path('home', HomeView.as_view(), name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('menu/', CategoryItems.as_view(), name='menu'),
     path('detail_item/<int:pk>', DetailItemView.as_view(), name='detail_item'),
 
