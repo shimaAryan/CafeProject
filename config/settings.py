375be9d183ed19cddb325df597f19a9aad0d305c
Sahar Mahmoudishad <sahar.mahmoodi01@gmail.com>
2023-12-11 12:52:14 +0330

    fix:debuging in setting

--- config/settings.py
+++ config/settings.py
@@ -80,13 +80,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+      'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'Coffee',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': 'Shad_M72770',
         'HOST': 'localhost',
         'PORT': '5432',
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
